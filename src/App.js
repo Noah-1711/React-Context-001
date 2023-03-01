39267326981ad471ae99f84dc118425f7f4aafe1
Noah-1711 <noahnirmal11@gmail.com>
2023-03-02 01:31:51 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { createContext, useState } from 'react';
 import './App.css';
 import { Content } from './Components/Content';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from './Components/Navbar';
+import { About } from './Components/About';
+import { Button } from './Components/Button';
+import { Services } from './Components/Services';
+
+
+
 
 export const Mycontext = createContext()
 
@@ -10,9 +18,20 @@ function App() {
 
   return (
     <div className="App">
-      <Mycontext.Provider value={[theme,setTheme,value1]}>
-      <Content />
+      <Mycontext.Provider value={{theme,setTheme,value1}}>
+      <Navbar />
+      <Button />
+      <Routes>
+        <Route path='/' element={<Content />}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/services' element={<Services/>}></Route>
+
+
+      
+  
+      </Routes>
       </Mycontext.Provider>
+  
     
     </div>
   );
